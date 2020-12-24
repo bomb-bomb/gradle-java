b10c9fc0c71cf175afb2ea7dc7aaf6fd2cb80fc6
chenguishan <3003876384@qq.com>
2020-12-24 15:25:11 +0800

    emmmm

---
+++ src/main/java/com/example/gradle/gradledemo/FileUploadController.java
@@ -0,0 +1,67 @@
+package com.example.gradle.gradledemo;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+import com.example.gradle.gradledemo.storage.FileSystemStorageService;
+import com.example.gradle.gradledemo.storage.StorageFileNotFoundException;
+import com.example.gradle.gradledemo.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping(path="/upload")
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(FileSystemStorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping(path = "/list")
+    public String listUploadedFiles(Model model) throws IOException {
+        System.out.println("===================================================================");
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toUri().toString())
+                .collect(Collectors.toList()));
+
+        return "uploadForm";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+}
--- src/main/java/com/example/gradle/gradledemo/GradledemoApplication.java
+++ src/main/java/com/example/gradle/gradledemo/GradledemoApplication.java
@@ -1,13 +1,55 @@
 package com.example.gradle.gradledemo;
 
+import com.example.gradle.gradledemo.models.Quote;
+import com.example.gradle.gradledemo.storage.FileSystemStorageService;
+import com.example.gradle.gradledemo.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+import org.springframework.boot.CommandLineRunner;
+
+@SuppressWarnings("ALL")
 @SpringBootApplication
+@EnableScheduling
+@EnableConfigurationProperties(StorageProperties.class)
 public class GradledemoApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(GradledemoApplication.class);
 
 	public static void main(String[] args) {
 		System.setProperty("spring.devtools.restart.enabled", "true");
 		SpringApplication.run(GradledemoApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	CommandLineRunner init(FileSystemStorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+
+			log.info(quote != null ? quote.toString() : null);
+		};
 	}
 }
