ef49b8c579023a6e83b078d0b3704fa69c3870af
chenguishan <3003876384@qq.com>
2021-01-05 15:00:33 +0800

    emmmm *6

--- src/main/java/com/example/gradle/gradledemo/accessingdatamysql/User.java
+++ src/main/java/com/example/gradle/gradledemo/accessingdatamysql/User.java
@@ -1,29 +1,24 @@
 package com.example.gradle.gradledemo.accessingdatamysql;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.github.braisdom.objsql.annotations.DomainModel;
+import com.github.braisdom.objsql.annotations.PrimaryKey;
+import com.github.braisdom.objsql.annotations.Queryable;
+import org.springframework.core.annotation.AliasFor;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity // This tells Hibernate to make a table out of this class
-@Setter
-@Getter
+@DomainModel
 public class User {
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
-
+    @Queryable
     @Size(min = 2, max = 5, message = "名字字数在2-5间")
     @NotNull
     private String name;
 
-    @Size(min = 2, max = 3, message = "邮箱字数在2-5间")
+    @Size(min = 2, max = 20, message = "邮箱字数在2-20间")
     @Email(message = "email error")
     private String email;
 }
